a66b45c0da8dd127e14ea347090e635a06b597da
Flexberry-man <mail@flexberry.net>
2023-12-15 19:11:02 +0000

    Flexberry code generation

--- src/ember-app/app/controllers/application.js
+++ src/ember-app/app/controllers/application.js
@@ -28,48 +28,49 @@ export default Controller.extend({
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-колонка-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-колонка-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-колонка-l.title'),
-            icon: 'edit',
+            icon: 'archive',
             children: null
           }, {
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-банк-данные-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-банк-данные-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-банк-данные-l.title'),
-            icon: 'address card',
+            icon: 'tasks',
             children: null
           }, {
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-клиент-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-клиент-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-клиент-l.title'),
-            icon: 'tags',
+            icon: 'chart line',
             children: null
           }, {
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-чек-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-чек-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-чек-l.title'),
-            icon: 'phone',
+            icon: 'address card',
             children: null
           }, {
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-заказ-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-заказ-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-заказ-l.title'),
-            icon: 'folder',
+            icon: 'address card',
             children: null
           }, {
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-товар-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-товар-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-товар-l.title'),
+            icon: 'book',
             children: null
           }, {
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-заправка-авто-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-заправка-авто-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-заправка-авто-l.title'),
-            icon: 'table',
+            icon: 'suitcase',
             children: null
           }, {
             link: 'i-i-s-k-u-r-s-a-v-a-y-a-заправ-бак-l',
             caption: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-заправ-бак-l.caption'),
             title: i18n.t('forms.application.sitemap.k-u-r-s-a-v-a-y-a.i-i-s-k-u-r-s-a-v-a-y-a-заправ-бак-l.title'),
-            icon: 'tags',
+            icon: 'building',
             children: null
           }]
         }
